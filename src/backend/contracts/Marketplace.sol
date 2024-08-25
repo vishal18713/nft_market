6bec9f974d8ba546932387742f24ab453c2afda3
vishal18713 <lcs2023052@iiitl.ac.in>
2024-08-25 14:30:16 +0530

    chore: Add makeItems function to Marketplace contract

--- src/backend/contracts/Marketplace.sol
+++ src/backend/contracts/Marketplace.sol
@@ -5,15 +5,40 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Marketplace is ReentrancyGuard {
-
     address payable public immutable feeAccount; // account for recciving fees
-    uint public immutable feepercent;//fee percentages on sales
+    uint public immutable feepercent; //fee percentages on sales
     uint public itemCount;
+    struct Item {
+        uint itemId;
+        IERC721 nft;
+        uint tokenId;
+        uint price;
+        address payable seller;
+        bool sold;
+    }
+    mapping(uint => Item) public items;
 
     constructor(uint _feePercent) {
         feeAccount = payable(msg.sender);
         feepercent = _feePercent;
-        
+    }
+
+    function makeItems(
+        IERC721 _nft,
+        uint _tokenId,
+        uint _price
+    ) external nonReentrant {
+        require(_price > 0, "Price must be greater than 0");
+        itemCount++;
+        _nft.transferFrom(msg.sender, address(this), _tokenId);
+
+        items[itemCount] = Item(
+            itemCount,
+            _nft,
+            _tokenId,
+            _price,
+            payable(msg.sender),
+            false
+        );
     }
 }
-
